a566aebf80b6e21b598851c10e1b3def18543179
Th3-B00g3ym4n <119944504+Th3-B00g3ym4n@users.noreply.github.com>
2022-12-06 15:37:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-from my_pip_package import __version__
+from pydriver import __version__
 
 setup(
-    name='my_pip_package',
+    name='pydriver',
     version=__version__,
 
-    url='https://github.com/MichaelKim0407/tutorial-pip-package',
+    url='https://github.com/Th3-B00g3ym4n/pydriver',
     author='The_Boogeyman',
     author_email='HAHAHA',
 
